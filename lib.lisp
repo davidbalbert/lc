c021eb44c8f6a899b2a797ecf4fdff3ccae2ff67
David Albert <davidbalbert@gmail.com>
2021-11-01 15:27:41 -0400

    letrec

--- lib.lisp
+++ lib.lisp
@@ -1,5 +1,8 @@
 (def caar (x) (car (car x)))
+(def cadr (x) (car (cdr x)))
 (def cadar (x) (car (cdr (car x))))
+
+(def list args)
 
 (def map (f l)
     (if (pair? l)
@@ -14,3 +17,15 @@
         `((fn () ,@body))
         `((fn (,(caar bindings))
             (let* ,(cdr bindings) . ,body)) ,(cadar bindings)))))
+
+(def repeat (v n)
+    (if (= n 0)
+        nil
+        (cons v (repeat v (- n 1)))))
+
+(def letrec (macro (bindings . body)
+    `(
+        (fn ,(map car bindings)
+            ,@(map (fn (b) `(set ,(car b) ,(cadr b))) bindings)
+            ,@body)
+        ,@(repeat nil (length bindings)))))
